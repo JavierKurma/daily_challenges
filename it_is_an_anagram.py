7be6380cf5e8c189add5507b2d58ad592f5b76e2
Facundo Aguirre <facundo.aguirrelaboral@gmail.com>
2026-03-12 11:25:56 -0300

    Cleanning the words froma non alpha num char

--- it_is_an_anagram.py
+++ it_is_an_anagram.py
@@ -7,8 +7,19 @@ true or false (Bool) depending on whether they are anagrams or not.
 - Two exactly identical words are not an anagram.
 
 """
-def anagram(word_1,word_2)->bool:
-
-    if len(word_1)==len(word_2):
-        word_1=word_1.lower()
-        
+def anagram(word_1,word_2):
+    word_1=word_1.lower()
+    word_2=word_2.lower()
+    clean_1=""
+    clean_2=""
+    for c in word_1:
+        if c.isalnum():
+            clean_1+=c
+    for c in word_2:
+        if c.isalnum():
+            clean_2+=c
+    if len(clean_1)==len(clean_2):
+        pass
+    else:
+        return False
+anagram("Facundo....    aguirre...12312","abc")
