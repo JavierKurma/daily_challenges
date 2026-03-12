9544ecde4f9fa110b3c77f390b7fcc0ba44724ee
Facundo Aguirre <facundo.aguirrelaboral@gmail.com>
2026-03-12 12:06:29 -0300

    Created a function that return the n first numbers of the serial of fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+"""
+Write a program that prints the first 50 numbers of the Fibonacci
+sequence starting from 0.
+- The Fibonacci series is composed of a sequence of numbers in
+  which the next one is always the sum of the previous two.
+  0, 1, 1, 2, 3, 5, 8, 13...
+
+"""
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print("Cuantos numeros de Fibonacci desea ver?")
+x=int(input("->"))
+while not isinstance(x,int):
+    print("Ingrese un numero")
+    x=int(input("->"))
+for i in range(x):
+    if i==x-1:
+        print(fibonacci(i))
+    else:
+        print(fibonacci(i), end=", ")
+
