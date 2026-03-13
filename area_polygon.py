5853b252e277ff16ae6ba42687eebccf89914c97
Facundo Aguirre <facundo.aguirrelaboral@gmail.com>
2026-03-13 06:46:30 -0300

    Made a Exception handelling and completed the code

--- area_polygon.py
+++ area_polygon.py
@@ -12,4 +12,29 @@ def area_polygon(base,height,sides)->float:
     else:
         return base*height
 print("Input the base and height of a rectangle or triangle: ")
-while
+while True:
+    try:
+        base=int(input("base-> "))
+        break
+    except ValueError:
+        print("Input a valid number")
+while True:
+    try:
+        height=int(input("height-> "))
+        break
+    except ValueError:
+        print("Input a valid number")
+while True:
+    try:
+        side=int(input("side-> "))
+        if side ==2 or side ==3:
+            break
+        else:
+            print("Input 2 or 3")
+            continue
+        break
+    except ValueError:
+        print("Input a valid number of sides")
+
+area=area_polygon(base,height,side)
+print(f"The area of the polygon with (base,height)=({base},{height}) is {area}")
