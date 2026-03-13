1abdf9d6680737ffb1e2a8d42c76e8951611399e
Facundo Aguirre <facundo.aguirrelaboral@gmail.com>
2026-03-13 06:17:16 -0300

    Develop the function to verify prime numbers and print the n first prime numbers

--- prime_numbers.py
+++ prime_numbers.py
@@ -3,7 +3,7 @@ Write a program that checks whether a number is prime or not.
 Once this is done, print the prime numbers between 1 and 100.
 """
 """
--Los numeros primos son numeros mayores que 1 que son unicamente divisibles entre 1 y si mismos
+-A prime number is all that is greatter than 1 and it only have %0 by 1 and itself
 """
 def is_prime_numbers(n)->bool:
     if n<=1:
@@ -12,3 +12,21 @@ def is_prime_numbers(n)->bool:
         if n%i==0:
             return False
     return True
+print("Input the range that you want to veriify prime numbers:")
+x=(input("->"))
+while True:
+    try:
+        x=int(x)
+        break
+    except:
+        while not isinstance(x,int):
+            print("Ingrese un numero entero")
+            x=(input("->"))
+for i in range(x):
+    if is_prime_numbers(i):
+        print (f"{i}",end=" ")
+print("")
+
+#Prime numbers are important for cibersecurity on encriptation
+
+
