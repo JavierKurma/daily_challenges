2ec781c9884f7af67ec41f30cdf2eb88852bc6f7
Facundo Aguirre <facundo.aguirrelaboral@gmail.com>
2026-03-13 06:50:55 -0300

    Grammar corrections

--- area_polygon.py
+++ area_polygon.py
@@ -11,30 +11,30 @@ def area_polygon(base,height,sides)->float:
         return base*height/2
     else:
         return base*height
-print("Input the base and height of a rectangle or triangle: ")
+print("Enter the base and height of a rectangle or triangle: ")
 while True:
     try:
         base=int(input("base-> "))
         break
     except ValueError:
-        print("Input a valid number")
+        print("Enter a valid number")
 while True:
     try:
         height=int(input("height-> "))
         break
     except ValueError:
-        print("Input a valid number")
+        print("Enter a valid number")
 while True:
     try:
         side=int(input("side-> "))
         if side ==2 or side ==3:
             break
         else:
-            print("Input 2 or 3")
+            print("Enter 2 or 3")
             continue
         break
     except ValueError:
-        print("Input a valid number of sides")
+        print("Enter a valid number of sides")
 
 area=area_polygon(base,height,side)
 print(f"The area of the polygon with (base,height)=({base},{height}) is {area}")
