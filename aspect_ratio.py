a7537f23d8ca98f434f93becfa18f07d733d553c
Facundo Aguirre <facundo.aguirrelaboral@gmail.com>
2026-03-19 21:29:03 -0300

    Cleaning code

--- aspect_ratio.py
+++ aspect_ratio.py
@@ -11,21 +11,13 @@ import requests
 from PIL import Image
 from io import BytesIO
 import math
-
-url= "https://i.pinimg.com/originals/49/dc/85/49dc8564e17e811fcbcfe076144d5082.jpg"
-
+url= "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png"
 response=requests.get(url)
-
 image=Image.open(BytesIO(response.content))
-
 width,height=image.size
-print(width)
-print(height)
 gcd=math.gcd(height,width)
 height=height//gcd
 width=width//gcd
-
-print(f"The ratio from the image is {width}:{height}")
-#image.show()
+print(f"Aspect Ratio={width}:{height}")
 
 
